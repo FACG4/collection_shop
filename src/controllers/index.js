ed94269e4bf95789624972e5a3c66599e6943064
InassTubail <anoos.hanii@gmail.com>
2018-05-02 14:03:36 +0300

    structute to home page and fixed navbar and footer

---
+++ src/controllers/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const home = require('./home');
+
+router.get('/', home.get);
+
+
+module.exports = router;
