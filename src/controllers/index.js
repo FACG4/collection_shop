bd6292bb74fe9f8b7281dfd3b1e55c5c59d7c7e2
InassTubail <anoos.hanii@gmail.com>
2018-05-03 16:48:33 +0300

    log in function

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const home = require('./home_page');
 const signup = require('./signup');
 const admin = require('./admin');
+const login = require('./login');
+
+
 
 const user_page = require('./user_page');
 const error = require('./error');
@@ -12,15 +15,20 @@ const error = require('./error');
 
 router.get('/', home.get);
 router.get('/signup',signup.get);
-router.post('/login',signup.post);
+router.post('/signup',signup.post);
 router.get('/user_page', user_page.get);
 
 
 router.post('/admin',admin.post);
 router.get('/admin', admin.get);
 
-router.use(error.client);
-router.use(error.server);
+
+router.get('/login', login.get);
+router.post('/login' , login.post);
+
+
+// router.use(error.client);
+// // router.use(error.server);
 
 
 
